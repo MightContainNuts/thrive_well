24e3fd61326d1b1c51dbce6191ada8b0c38bea04
MightContainNuts <mightcontainnuts@icloud.com>
2025-02-16 06:05:47 +0100

    restructured file structure

--- application/app_factory.py
+++ application/app_factory.py
@@ -1,27 +1,37 @@
 from flask import Flask
 from application.config import config
+from application.db.db_init import db
 import os
+import logging
+import sys
 from application.views.main_views import main
-from application.logger import setup_logger
-
-
-logger = setup_logger(__name__)
 
 
 def create_app():
     app = Flask(__name__)
+
     config_name = os.environ.get("FLASK_ENV", "development")
+    app.config.from_object(
+        config[config_name]()
+    )  # Assuming config handles db URL and other settings
 
-    if config_name not in config:
-        raise ValueError(
-            f"Invalid FLASK_ENV '{config_name}'. Choose from: {list(config.keys())}"  # noqa E501
-        )
+    # Initialize any app extensions, e.g., database
+    db.init_app(app)
+    app.logger.info("Database connection established")
 
-    app.config.from_object(config[config_name]())
-    config[config_name]().init_app(app)
-    app.register_blueprint(main)
+    # Configure logging
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(
+        app.config.get("LOG_LEVEL", logging.DEBUG)
+    )  # Default to DEBUG if not set in config
 
-    print("Config name:", config_name)
-    print("Available configs:", list(config.keys()))
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+
+    app.logger.addHandler(handler)
+
+    app.logger.info(f"App started - Config name: {config_name}")
+
+    app.register_blueprint(main)  # Registering the main blueprint after config
 
     return app
--- application/config.py
+++ application/config.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 base_dir = Path(__name__).parent.parent
 
 
 class Config:
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     @staticmethod
     def init_app(app):
@@ -15,17 +20,16 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
     # add local database
 
 
 class TestConfig(Config):
     DEBUG = True
-    # add local database
 
 
 class ProductionConfig(Config):
     DEBUG = True
-    # add remote db
 
 
 config = {
